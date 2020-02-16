0215bfafec492e6e22d6f9dc493c4cf8135bb1f9
artterapevt <61111303+artterapevt@users.noreply.github.com>
2020-02-16 15:22:51 +0200

    Add La Loba and a half of Pryatki cards pack

---
+++ data/cards.js
@@ -0,0 +1,366 @@
+
+const words1 = ["Баловаться", "Богатство", "Благодарность", "Верить", "Высота", "Встреча", "Вдохновение", "Взгляд", "Говорить", "Главный", "Горизонт", "Гибкость", "Гордость", "Граница", "Доверять", "Делать", "Делиться", "Единственный", "Ждать", "Забота", "Защищать", "Задумка", "Источник", "Искривление", "Искуственный", "Изменить", "Креатив", "Круг", "Летать", "Любовь", "Маскировать", "Мягкий", "Находить", "Настоящий", "Нить", "Напряжение", "Наслаждаться", "Наблюдать", "Навязчивость", "Неизвестность", "Обновлять", "Отдых", "Отказ", "Опасность", "Обман", "Прятать", "Принятие", "Плыть", "Придумка", "Помнить", "Прислушиваться", "Проверять", "Порядок", "Поддержка", "Приобретать", "Путь", "Пустота", "Реальность", "Рост", "Развод", "Руководить", "Разрешать", "Резать", "Решение", "Строить", "Слой", "Создавать", "Смех", "След", "Стремление", "Спокойствие", "Сила", "Свет", "Сладость", "Свобода", "Тренировка", "Тень", "Топтать", "Трещина", "Тепло", "Творить", "Уважать", "Угол", "Фантастика", "Хотеть", "Холод", "Хранить", "Ценность", "Чувствовать"];
+const imagesPath = '../assets/images/card_packs/';
+
+export const cards = [
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/1.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/2.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/3.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/4.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/5.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/6.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/7.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/8.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/9.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/10.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/11.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/12.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/13.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/14.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/15.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/16.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/17.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/18.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/19.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/20.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/21.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/22.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/23.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/24.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/25.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/26.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/27.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/28.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/29.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/30.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/31.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/32.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/33.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/34.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/35.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/36.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/37.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/38.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/39.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/40.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/41.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/42.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/43.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/44.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/45.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/46.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/47.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/48.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/49.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/50.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/51.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/52.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/53.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/54.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/55.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/56.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/57.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/58.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/59.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/60.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/61.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/62.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/63.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/64.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/65.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/66.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/67.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/68.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/69.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/70.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/71.jpg`,
+        "description": words1
+    },
+    {
+        "type": "Прятки",
+        "href": `${imagesPath}Прятки/72.jpg`,
+        "description": words1
+    }
+];
